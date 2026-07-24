ddf40fef9aee4158fb486680418f8186ac5609e5
8vz4t865m8-sudo <8vz4t865m8@privaterelay.appleid.com>
2026-07-25 06:40:41 +0800

    Update iphook.m

--- iphook.m
+++ iphook.m
@@ -1,7 +1,6 @@
 //
-//  iphook.m - KFun 卡密验证 Bypass (最简版)
-//  策略: 只 Hook showError:，让错误提示变成成功，然后移除遮罩进入主界面
-//  不 Hook onTapVerify 和 activateCode，让原逻辑正常走
+//  iphook.m - 测试版
+//  只 Hook showError: 和加弹窗测试
 //
 
 #import <UIKit/UIKit.h>
@@ -10,100 +9,69 @@
 
 #define LOG(fmt, ...) NSLog(@"[IPH] " fmt, ##__VA_ARGS__)
 
-static void hideMaskAndEnterMain(id self) {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        // 隐藏遮罩
-        id mask = nil;
-        @try { mask = [self valueForKey:@"authMaskView"]; } @catch (NSException *e) {}
-        if (mask) {
-            [mask setValue:@YES forKey:@"hidden"];
-            [(UIView *)mask setUserInteractionEnabled:NO];
-            [(UIView *)mask removeFromSuperview];
-        }
-        // 隐藏错误标签
-        id errorLabel = nil;
-        @try { errorLabel = [self valueForKey:@"errorLabel"]; } @catch (NSException *e) {}
-        if (errorLabel) {
-            [errorLabel setValue:@"" forKey:@"text"];
-            [(UIView *)errorLabel setHidden:YES];
-        }
-        // 尝试进入主界面
-        if ([self respondsToSelector:@selector(setupAfterActivation)]) {
-            ((void(*)(id, SEL))objc_msgSend)(self, @selector(setupAfterActivation));
-        }
-    });
-}
-
-static void hook_showError(id self, SEL _cmd, NSString *msg) {
-    LOG(@"拦截错误: %@，直接放行", msg);
-    // 不显示错误，直接移除遮罩进入主界面
-    hideMaskAndEnterMain(self);
-}
-
-static void hook_activateCode(id self, SEL _cmd, NSString *code, void (^completion)(BOOL success, id data)) {
-    LOG(@"Bypass activateCode: %@", code);
-    // 直接回调成功
-    if (completion) {
-        completion(YES, @{
-            @"code": @0,
-            @"msg": @"success",
-            @"data": @{
-                @"expire": @"2099-12-31 23:59:59",
-                @"type": @"lifetime"
+// 弹窗测试
+static void showTestAlert() {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"[IPH] 测试"
+                                                                       message:@"dylib已加载"
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil]];
+        
+        UIWindow *window = nil;
+        for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
+            if ([scene isKindOfClass:[UIWindowScene class]]) {
+                window = scene.keyWindow;
+                break;
             }
-        });
-    }
-    // 延迟移除遮罩
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1.0 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-        hideMaskAndEnterMain(self);
+        }
+        if (!window) window = [UIApplication sharedApplication].keyWindow;
+        if (window && window.rootViewController) {
+            [window.rootViewController presentViewController:alert animated:YES completion:nil];
+        }
     });
 }
 
-static void hook_onTapVerify(id self, SEL _cmd) {
-    LOG(@"Bypass onTapVerify");
-    // 直接显示成功弹窗
-    if ([self respondsToSelector:@selector(buildSuccessViewWithExpire:)]) {
-        ((void(*)(id, SEL, NSString *))objc_msgSend)(self, @selector(buildSuccessViewWithExpire:), @"2099-12-31 23:59:59");
-    }
-    // 延迟移除遮罩进入主界面
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1.5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-        hideMaskAndEnterMain(self);
+// Hook showError:
+static void hook_showError(id self, SEL _cmd, NSString *msg) {
+    LOG(@"[TEST] showError被调用: %@", msg);
+    // 不显示错误，改为弹窗提示
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"[IPH] 拦截"
+                                                                       message:[NSString stringWithFormat:@"原错误: %@", msg]
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil]];
+        
+        UIWindow *window = nil;
+        for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
+            if ([scene isKindOfClass:[UIWindowScene class]]) {
+                window = scene.keyWindow;
+                break;
+            }
+        }
+        if (!window) window = [UIApplication sharedApplication].keyWindow;
+        if (window && window.rootViewController) {
+            [window.rootViewController presentViewController:alert animated:YES completion:nil];
+        }
     });
-}
-
-static void hook_checkTask(id self, SEL _cmd) {
-    // 空实现
-}
-
-static void hook_shakeField(id self, SEL _cmd) {
-    // 空实现
 }
 
 static void doInit() {
     LOG(@"开始 Hook...");
+    
     Class cls = objc_getClass("WWWActivationViewController");
-    if (!cls) { LOG(@"找不到类!"); return; }
+    if (!cls) {
+        LOG(@"找不到 WWWActivationViewController!");
+        return;
+    }
+    LOG(@"找到类: WWWActivationViewController");
     
-    Method m;
-    
-    // 只 Hook showError:，让任何错误都变成放行
-    m = class_getInstanceMethod(cls, @selector(showError:));
-    if (m) { method_setImplementation(m, (IMP)hook_showError); LOG(@"Hooked showError:"); }
-    
-    // Hook activateCode:completion: 作为备用
-    m = class_getInstanceMethod(cls, @selector(activateCode:completion:));
-    if (m) { method_setImplementation(m, (IMP)hook_activateCode); LOG(@"Hooked activateCode:completion:"); }
-    
-    // Hook onTapVerify 作为备用
-    m = class_getInstanceMethod(cls, @selector(onTapVerify));
-    if (m) { method_setImplementation(m, (IMP)hook_onTapVerify); LOG(@"Hooked onTapVerify"); }
-    
-    // Hook checkTask 和 shakeField
-    m = class_getInstanceMethod(cls, @selector(checkTask));
-    if (m) { method_setImplementation(m, (IMP)hook_checkTask); LOG(@"Hooked checkTask"); }
-    
-    m = class_getInstanceMethod(cls, @selector(shakeField));
-    if (m) { method_setImplementation(m, (IMP)hook_shakeField); LOG(@"Hooked shakeField"); }
+    Method m = class_getInstanceMethod(cls, @selector(showError:));
+    if (m) {
+        method_setImplementation(m, (IMP)hook_showError);
+        LOG(@"Hooked showError:");
+    } else {
+        LOG(@"找不到 showError:");
+    }
     
     LOG(@"初始化完成");
 }
@@ -111,8 +79,13 @@ static void doInit() {
 __attribute__((constructor))
 static void iphook_init() {
     LOG(@"========================================");
-    LOG(@"KFun Bypass 已加载");
+    LOG(@"KFun Bypass 测试版 已加载");
     LOG(@"========================================");
+    
+    // 弹窗测试
+    showTestAlert();
+    
+    // 延迟 Hook
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         doInit();
     });
