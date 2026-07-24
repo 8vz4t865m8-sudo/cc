0a2f288649a60b458ecbaa7a6c3b6304f3708ff2
8vz4t865m8-sudo <8vz4t865m8@privaterelay.appleid.com>
2026-07-25 06:17:13 +0800

    Update iphook.m

--- iphook.m
+++ iphook.m
@@ -1,7 +1,6 @@
 //
 //  iphook.m - KFun 卡密验证 Bypass
 //  目标: WWWActivationViewController
-//  策略: 只 Hook activateCode:completion:，直接回调成功
 //
 
 #import <UIKit/UIKit.h>
@@ -10,78 +9,52 @@
 
 #define LOG(fmt, ...) NSLog(@"[IPH] " fmt, ##__VA_ARGS__)
 
-// ============================================================
-// 隐藏遮罩
-// ============================================================
-static void hideMask(id self) {
-    id mask = nil;
-    @try { mask = [self valueForKey:@"authMaskView"]; } @catch (NSException *e) {}
-    if (!mask) @try { mask = [self valueForKey:@"_authMaskView"]; } @catch (NSException *e) {}
-
-    if (mask) {
-        dispatch_async(dispatch_get_main_queue(), ^{
+static void hideAuthUI(id self) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        id mask = nil;
+        @try { mask = [self valueForKey:@"authMaskView"]; } @catch (NSException *e) {}
+        if (mask) {
             [mask setValue:@YES forKey:@"hidden"];
             [(UIView *)mask setUserInteractionEnabled:NO];
             [(UIView *)mask removeFromSuperview];
-            LOG(@"遮罩已移除");
-        });
-    }
-}
-
-// ============================================================
-// 尝试进入主界面
-// ============================================================
-static void tryEnterMain(id self) {
-    // 先尝试 self 上的方法
-    if ([self respondsToSelector:@selector(setupAfterActivation)]) {
-        LOG(@"调用 setupAfterActivation");
-        dispatch_async(dispatch_get_main_queue(), ^{
-            ((void(*)(id, SEL))objc_msgSend)(self, @selector(setupAfterActivation));
-        });
-        return;
-    }
-
-    // 尝试从当前 VC 找
-    dispatch_async(dispatch_get_main_queue(), ^{
-        id vc = nil;
-        @try {
-            UIWindowScene *scene = nil;
-            for (UIWindowScene *s in [UIApplication sharedApplication].connectedScenes) {
-                if ([s isKindOfClass:[UIWindowScene class]]) { scene = s; break; }
-            }
-            UIWindow *window = scene ? scene.keyWindow : [UIApplication sharedApplication].keyWindow;
-            vc = window.rootViewController;
-            while (vc && [vc respondsToSelector:@selector(presentedViewController)] && [vc presentedViewController])
-                vc = [vc presentedViewController];
-        } @catch (NSException *e) { return; }
-
-        if (!vc) return;
-
-        if ([vc respondsToSelector:@selector(setupAfterActivation)]) {
-            LOG(@"从 VC 调用 setupAfterActivation");
-            ((void(*)(id, SEL))objc_msgSend)(vc, @selector(setupAfterActivation));
-            return;
         }
-
-        for (id child in [vc valueForKey:@"childViewControllers"]) {
-            if ([child respondsToSelector:@selector(setupAfterActivation)]) {
-                LOG(@"从子控制器调用 setupAfterActivation");
-                ((void(*)(id, SEL))objc_msgSend)(child, @selector(setupAfterActivation));
-                return;
-            }
+        id errorLabel = nil;
+        @try { errorLabel = [self valueForKey:@"errorLabel"]; } @catch (NSException *e) {}
+        if (errorLabel) {
+            [errorLabel setValue:@"" forKey:@"text"];
+            [(UIView *)errorLabel setHidden:YES];
+        }
+        id successView = nil;
+        @try { successView = [self valueForKey:@"successView"]; } @catch (NSException *e) {}
+        if (successView) {
+            [successView setValue:@YES forKey:@"hidden"];
+            [(UIView *)successView removeFromSuperview];
         }
     });
 }
 
-// ============================================================
-// Hook 实现
-// ============================================================
+static void tryEnterMain(id self) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if ([self respondsToSelector:@selector(setupAfterActivation)]) {
+            ((void(*)(id, SEL))objc_msgSend)(self, @selector(setupAfterActivation));
+            return;
+        }
+        UIWindowScene *scene = nil;
+        for (UIWindowScene *s in [UIApplication sharedApplication].connectedScenes) {
+            if ([s isKindOfClass:[UIWindowScene class]]) { scene = s; break; }
+        }
+        UIWindow *window = scene ? scene.keyWindow : [UIApplication sharedApplication].keyWindow;
+        id vc = window.rootViewController;
+        while (vc && [vc respondsToSelector:@selector(presentedViewController)] && [vc presentedViewController])
+            vc = [vc presentedViewController];
+        if (vc && [vc respondsToSelector:@selector(setupAfterActivation)]) {
+            ((void(*)(id, SEL))objc_msgSend)(vc, @selector(setupAfterActivation));
+        }
+    });
+}
 
-static void hook_activateCode(id self, SEL _cmd, NSString *code, id completion) {
-    LOG(@"Bypass activateCode: %@", code);
-
-    // 伪造成功数据
-    NSDictionary *fakeData = @{
+static NSDictionary *fakeSuccessData() {
+    return @{
         @"code": @0,
         @"msg": @"success",
         @"data": @{
@@ -89,56 +62,113 @@ static void hook_activateCode(id self, SEL _cmd, NSString *code, id completion) 
             @"type": @"lifetime"
         }
     };
+}
 
-    // 回调 - 用 id 类型避免 block 签名不匹配
-    if (completion) {
-        @try {
-            // 尝试各种 block 签名
-            void (^block1)(BOOL, id) = completion;
-            block1(YES, fakeData);
-        } @catch (NSException *e1) {
-            @try {
-                void (^block2)(BOOL, NSDictionary *) = completion;
-                block2(YES, fakeData);
-            } @catch (NSException *e2) {
-                @try {
-                    void (^block3)(id, id) = completion;
-                    block3(fakeData, nil);
-                } @catch (NSException *e3) {
-                    LOG(@"Block 回调失败: %@", e3);
-                }
-            }
-        }
-    }
-
-    // 移除遮罩
-    hideMask(self);
-
-    // 尝试进入主界面
+static void hook_activateCode(id self, SEL _cmd, NSString *code, void (^completion)(BOOL success, id data)) {
+    LOG(@"Bypass activateCode: %@", code);
+    if (completion) completion(YES, fakeSuccessData());
+    hideAuthUI(self);
     tryEnterMain(self);
 }
 
-// ============================================================
-// 初始化
-// ============================================================
+static void hook_verifyWithCompletion(id self, SEL _cmd, void (^completion)(BOOL success)) {
+    LOG(@"Bypass verifyWithCompletion");
+    if (completion) completion(YES);
+    hideAuthUI(self);
+    tryEnterMain(self);
+}
+
+static void hook_showError(id self, SEL _cmd, NSString *msg) {
+    LOG(@"拦截错误: %@", msg);
+    if ([self respondsToSelector:@selector(showSuccess:completion:)]) {
+        ((void(*)(id, SEL, NSString *, void (^)(void)))objc_msgSend)(
+            self, @selector(showSuccess:completion:), @"验证成功", ^{});
+    }
+    hideAuthUI(self);
+    tryEnterMain(self);
+}
+
+static void hook_checkTask(id self, SEL _cmd) {}
+
+static void hook_shakeField(id self, SEL _cmd) {}
+
+static void hook_onTapVerify(id self, SEL _cmd) {
+    LOG(@"Bypass onTapVerify");
+    if ([self respondsToSelector:@selector(activateCode:completion:)]) {
+        ((void(*)(id, SEL, NSString *, void (^)(BOOL, id)))objc_msgSend)(
+            self, @selector(activateCode:completion:), @"BYPASSED", ^(BOOL success, id data) {});
+    }
+}
+
+static void hook_prefillCode(id self, SEL _cmd, NSString *code) {
+    LOG(@"拦截预填充: %@", code);
+    if ([self respondsToSelector:@selector(activateCode:completion:)]) {
+        ((void(*)(id, SEL, NSString *, void (^)(BOOL, id)))objc_msgSend)(
+            self, @selector(activateCode:completion:), @"BYPASSED", ^(BOOL success, id data) {});
+    }
+}
+
+static IMP orig_dtwr = NULL;
+
+typedef NSURLSessionDataTask *(*dtwr_t)(id, SEL, NSURLRequest *, void (^)(NSData *, NSURLResponse *, NSError *));
+
+static NSURLSessionDataTask *hook_dtwr(id self, SEL _cmd, NSURLRequest *request, void (^completion)(NSData *, NSURLResponse *, NSError *)) {
+    NSString *url = request.URL.absoluteString.lowercaseString;
+    NSString *body = nil;
+    if (request.HTTPBody) body = [[NSString alloc] initWithData:request.HTTPBody encoding:NSUTF8StringEncoding];
+    NSArray *kw = @[@"auth", @"verify", @"license", @"activate", @"check", @"login", @"key", @"code", @"card"];
+    BOOL isAuth = NO;
+    for (NSString *k in kw) {
+        if ([url containsString:k] || [body.lowercaseString containsString:k]) { isAuth = YES; break; }
+    }
+    if (isAuth) {
+        LOG(@"拦截验证请求: %@", request.URL.absoluteString);
+        if (completion) {
+            NSString *json = @"{\"code\":0,\"msg\":\"success\",\"data\":{\"expire\":\"2099-12-31 23:59:59\",\"type\":\"lifetime\"}}";
+            NSData *data = [json dataUsingEncoding:NSUTF8StringEncoding];
+            NSHTTPURLResponse *resp = [[NSHTTPURLResponse alloc] initWithURL:request.URL statusCode:200 HTTPVersion:@"HTTP/1.1" headerFields:@{@"Content-Type": @"application/json"}];
+            dispatch_async(dispatch_get_main_queue(), ^{ completion(data, resp, nil); });
+        }
+        NSURLSession *s = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+        return [s dataTaskWithURL:[NSURL URLWithString:@"http://localhost"]];
+    }
+    return ((dtwr_t)orig_dtwr)(self, _cmd, request, completion);
+}
+
 static void doInit() {
     LOG(@"开始 Hook...");
-
-    Class cls = objc_getClass("WWWActivationViewController");
-    if (!cls) {
-        LOG(@"找不到 WWWActivationViewController!");
-        return;
+    const char *clsName = "WWWActivationViewController";
+    Class cls = objc_getClass(clsName);
+    if (!cls) { LOG(@"找不到类!"); return; }
+    
+    Method m;
+    m = class_getInstanceMethod(cls, @selector(activateCode:completion:));
+    if (m) { method_setImplementation(m, (IMP)hook_activateCode); LOG(@"Hooked activateCode:completion:"); }
+    
+    m = class_getInstanceMethod(cls, @selector(verifyWithCompletion:));
+    if (m) { method_setImplementation(m, (IMP)hook_verifyWithCompletion); LOG(@"Hooked verifyWithCompletion:"); }
+    
+    m = class_getInstanceMethod(cls, @selector(showError:));
+    if (m) { method_setImplementation(m, (IMP)hook_showError); LOG(@"Hooked showError:"); }
+    
+    m = class_getInstanceMethod(cls, @selector(checkTask));
+    if (m) { method_setImplementation(m, (IMP)hook_checkTask); LOG(@"Hooked checkTask"); }
+    
+    m = class_getInstanceMethod(cls, @selector(shakeField));
+    if (m) { method_setImplementation(m, (IMP)hook_shakeField); LOG(@"Hooked shakeField"); }
+    
+    m = class_getInstanceMethod(cls, @selector(onTapVerify));
+    if (m) { method_setImplementation(m, (IMP)hook_onTapVerify); LOG(@"Hooked onTapVerify"); }
+    
+    m = class_getInstanceMethod(cls, @selector(prefillCode:));
+    if (m) { method_setImplementation(m, (IMP)hook_prefillCode); LOG(@"Hooked prefillCode:"); }
+    
+    Class sessionCls = objc_getClass("NSURLSession");
+    if (sessionCls) {
+        m = class_getInstanceMethod(sessionCls, @selector(dataTaskWithRequest:completionHandler:));
+        if (m) { orig_dtwr = method_getImplementation(m); method_setImplementation(m, (IMP)hook_dtwr); LOG(@"Hooked NSURLSession"); }
     }
-    LOG(@"找到类: WWWActivationViewController");
-
-    Method m = class_getInstanceMethod(cls, @selector(activateCode:completion:));
-    if (m) {
-        method_setImplementation(m, (IMP)hook_activateCode);
-        LOG(@"Hooked activateCode:completion:");
-    } else {
-        LOG(@"找不到 activateCode:completion:");
-    }
-
+    
     LOG(@"初始化完成");
 }
 
@@ -147,8 +177,6 @@ static void iphook_init() {
     LOG(@"========================================");
     LOG(@"KFun Bypass 已加载");
     LOG(@"========================================");
-
-    // 延迟初始化，确保 UIKit 准备好
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         doInit();
     });
